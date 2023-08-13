861f64afc4c7eac408d51312d97960aa37cde627
fwf_mac <1770194225@qq.com>
2023-08-13 16:45:25 +0800

    comment action test

--- ByteDanceTrain/douyin/test/interaction_comment_action_test.go
+++ ByteDanceTrain/douyin/test/interaction_comment_action_test.go
@@ -109,62 +109,109 @@ func TestCommentAction2Success(t *testing.T) {
 	}
 }
 
-func TestCommentActionParamsEmptyFail(t *testing.T) {
-	favoriteRebuild(t)
+func TestCommentAction2SafeSuccess(t *testing.T) {
+	commentRebuild(t)
+	videoID, actionType := 2, 2
 	payload := &bytes.Buffer{}
 	writer := multipart.NewWriter(payload)
 	_ = writer.WriteField("token", token)
-	_ = writer.WriteField("video_id", strconv.Itoa(2))
-	//_ = writer.WriteField("action_type", strconv.Itoa(1))
-	body := &pb.DouyinFavoriteActionResponse{}
-	postResponse(t, payload, writer, constants.RouteFavoriteAction, body)
+	_ = writer.WriteField("video_id", strconv.Itoa(videoID))
+	_ = writer.WriteField("action_type", strconv.Itoa(actionType))
+	_ = writer.WriteField("comment_id", strconv.Itoa(1)) // root's comment
+	body := &pb.DouyinCommentActionResponse{}
+	postResponse(t, payload, writer, constants.RouteCommentAction, body)
+	if *body.StatusCode != dyerror.DBDeleteCommentEventError.ErrCode ||
+		*body.StatusMsg != dyerror.DBDeleteCommentEventError.ErrMessage ||
+		!checkCommentEqual(body.Comment, constants.DefaultComment) {
+		t.Fatalf("Test results are not as expected: %v", body)
+	}
+}
+
+func TestCommentActionParamsEmptyFail(t *testing.T) {
+	commentRebuild(t)
+	videoID := 2
+	//commentText, commentDate := "抉择之战 傻逼游戏", time.Now().Format("01-02")
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	_ = writer.WriteField("token", token)
+	_ = writer.WriteField("video_id", strconv.Itoa(videoID))
+	_ = writer.WriteField("action_type", strconv.Itoa(1))
+	//_ = writer.WriteField("comment_text", commentText)
+	body := &pb.DouyinCommentActionResponse{}
+	postResponse(t, payload, writer, constants.RouteCommentAction, body)
 	if *body.StatusCode != dyerror.ParamEmptyError.ErrCode ||
-		*body.StatusMsg != dyerror.ParamEmptyError.ErrMessage {
+		*body.StatusMsg != dyerror.ParamEmptyError.ErrMessage ||
+		!checkCommentEqual(body.Comment, constants.DefaultComment) {
+		t.Fatalf("Test results are not as expected: %v", body)
+	}
+
+	payload = &bytes.Buffer{}
+	writer = multipart.NewWriter(payload)
+	_ = writer.WriteField("token", token)
+	_ = writer.WriteField("video_id", strconv.Itoa(videoID))
+	_ = writer.WriteField("action_type", strconv.Itoa(2))
+	//_ = writer.WriteField("comment_id", strconv.Itoa(int(*TestComment.Id)))
+	postResponse(t, payload, writer, constants.RouteCommentAction, body)
+	if *body.StatusCode != dyerror.ParamEmptyError.ErrCode ||
+		*body.StatusMsg != dyerror.ParamEmptyError.ErrMessage ||
+		!checkCommentEqual(body.Comment, constants.DefaultComment) {
 		t.Fatalf("Test results are not as expected: %v", body)
 	}
 }
 
 func TestCommentActionParamsInputTypeFail(t *testing.T) {
-	favoriteRebuild(t)
+	commentRebuild(t)
+	actionType := 1
+	commentText := "抉择之战 傻逼游戏"
 	payload := &bytes.Buffer{}
 	writer := multipart.NewWriter(payload)
 	_ = writer.WriteField("token", token)
 	_ = writer.WriteField("video_id", "a")
-	_ = writer.WriteField("action_type", strconv.Itoa(1))
-	body := &pb.DouyinFavoriteActionResponse{}
-	postResponse(t, payload, writer, constants.RouteFavoriteAction, body)
+	_ = writer.WriteField("action_type", strconv.Itoa(actionType))
+	_ = writer.WriteField("comment_text", commentText)
+	body := &pb.DouyinCommentActionResponse{}
+	postResponse(t, payload, writer, constants.RouteCommentAction, body)
 	if *body.StatusCode != dyerror.ParamInputTypeError.ErrCode ||
-		*body.StatusMsg != dyerror.ParamInputTypeError.ErrMessage {
+		*body.StatusMsg != dyerror.ParamInputTypeError.ErrMessage ||
+		!checkCommentEqual(body.Comment, constants.DefaultComment) {
 		t.Fatalf("Test results are not as expected: %v", body)
 	}
 }
 
 func TestCommentActionUnknownActionTypeFail(t *testing.T) {
-	favoriteRebuild(t)
+	commentRebuild(t)
+	videoID, actionType := 3, 3
+	commentText := "抉择之战 傻逼游戏"
 	payload := &bytes.Buffer{}
 	writer := multipart.NewWriter(payload)
 	_ = writer.WriteField("token", token)
-	_ = writer.WriteField("video_id", strconv.Itoa(2))
-	_ = writer.WriteField("action_type", strconv.Itoa(3))
-	body := &pb.DouyinFavoriteActionResponse{}
-	postResponse(t, payload, writer, constants.RouteFavoriteAction, body)
+	_ = writer.WriteField("video_id", strconv.Itoa(videoID))
+	_ = writer.WriteField("action_type", strconv.Itoa(actionType))
+	_ = writer.WriteField("comment_text", commentText)
+	body := &pb.DouyinCommentActionResponse{}
+	postResponse(t, payload, writer, constants.RouteCommentAction, body)
 	if *body.StatusCode != dyerror.ParamUnknownActionTypeError.ErrCode ||
-		*body.StatusMsg != dyerror.ParamUnknownActionTypeError.ErrMessage {
+		*body.StatusMsg != dyerror.ParamUnknownActionTypeError.ErrMessage ||
+		!checkCommentEqual(body.Comment, constants.DefaultComment) {
 		t.Fatalf("Test results are not as expected: %v", body)
 	}
 }
 
 func TestCommentActionAuthTokenFail(t *testing.T) {
-	favoriteRebuild(t)
+	commentRebuild(t)
+	videoID, actionType := 3, 1
+	commentText := "抉择之战 傻逼游戏"
 	payload := &bytes.Buffer{}
 	writer := multipart.NewWriter(payload)
 	_ = writer.WriteField("token", token[1:])
-	_ = writer.WriteField("video_id", strconv.Itoa(2))
-	_ = writer.WriteField("action_type", strconv.Itoa(1))
-	body := &pb.DouyinFavoriteActionResponse{}
-	postResponse(t, payload, writer, constants.RouteFavoriteAction, body)
+	_ = writer.WriteField("video_id", strconv.Itoa(videoID))
+	_ = writer.WriteField("action_type", strconv.Itoa(actionType))
+	_ = writer.WriteField("comment_text", commentText)
+	body := &pb.DouyinCommentActionResponse{}
+	postResponse(t, payload, writer, constants.RouteCommentAction, body)
 	if *body.StatusCode != dyerror.AuthTokenFailError.ErrCode ||
-		*body.StatusMsg != dyerror.AuthTokenFailError.ErrMessage {
+		*body.StatusMsg != dyerror.AuthTokenFailError.ErrMessage ||
+		!checkCommentEqual(body.Comment, constants.DefaultComment) {
 		t.Fatalf("Test results are not as expected: %v", body)
 	}
 }
