0dedfd98dee0cbdeba0cfb1469f2d4b4d8bc03f8
fwf_mac <1770194225@qq.com>
2023-08-16 08:46:11 +0800

    add a message events table

---
+++ ByteDanceTrain/douyin/assets/message_event.sql
@@ -0,0 +1,16 @@
+DROP TABLE MessageEvents;
+CREATE TABLE MessageEvents
+(
+    `id` INT UNSIGNED AUTO_INCREMENT COMMENT '消息 ID',
+    `to_user_id` INT UNSIGNED COMMENT '消息接受者的 ID',
+    `from_user_id` INT UNSIGNED COMMENT '消息发送者的 ID',
+    `content` VARCHAR(100) NOT NULL COMMENT '消息内容',
+    `create_time` VARCHAR(5) NOT NULL COMMENT '消息创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO MessageEvents  (`to_user_id`, `from_user_id`,`content`, `create_time`)
+VALUES (2, 1, '测试', '01-01');
+
+INSERT INTO MessageEvents  (`to_user_id`, `from_user_id`,`content`, `create_time`)
+VALUES (2, 3, '开学不快乐', '08-16');
--- ByteDanceTrain/douyin/model/entity/message.go
+++ ByteDanceTrain/douyin/model/entity/message.go
@@ -1,9 +1,13 @@
 package entity
 
 type Message struct {
-	Id         int64  `json:"id,omitempty"`           // 消息id
-	ToUserId   int64  `json:"to_user_id,omitempty"`   // 该消息接收者的id
-	FromUserId int64  `json:"from_user_id,omitempty"` // 该消息发送者的id
-	Content    string `json:"content,omitempty"`      // 消息内容
-	CreateTime string `json:"create_time,omitempty"`  // 消息创建时间
+	ID         int64  `gorm:"column:id;type:int(11) unsigned;primary_key;AUTO_INCREMENT;comment:消息 ID" json:"id"`
+	ToUserID   int64  `gorm:"column:to_user_id;type:int(11) unsigned;comment:消息接受者的 ID" json:"to_user_id"`
+	FromUserID int64  `gorm:"column:from_user_id;type:int(11) unsigned;comment:消息发送者的 ID" json:"from_user_id"`
+	Content    string `gorm:"column:content;type:varchar(100);comment:消息内容;NOT NULL" json:"content"`
+	CreateTime string `gorm:"column:create_time;type:varchar(5);comment:消息创建时间;NOT NULL" json:"create_time"`
+}
+
+func (Message) TableName() string {
+	return "MessageEvents"
 }
