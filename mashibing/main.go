db4115371a21f033323a5868816dca36d2749638
fwf windows <1770194225@qq.com>
2024-04-24 16:09:23 +0800

    finish mashibing class 5

--- mashibing/main.go
+++ mashibing/main.go
@@ -39,17 +39,26 @@ func getParams(ctx *gin.Context) {
 	ctx.String(http.StatusOK, "Get Param Key name = %s, age = %s", name, age)
 }
 
+func search(ctx *gin.Context) {
+	page := ctx.DefaultQuery("page", "default string")
+	key := ctx.PostForm("key")
+	age := ctx.PostForm("age")
+	hobby := ctx.PostFormArray("hobby")
+	ctx.String(http.StatusOK, "this is search() page=%s\tkey=%s\tage=%s\thobby=%v\tcount of hobby=%d", page, key, age, hobby, len(hobby))
+}
+
 func main() {
 	r := gin.Default()                     // 拿到一个 Engine 引擎 在New的基础上加入 Logger 与 Recovery 中间件
 	r.GET("ping", func(ctx *gin.Context) { // 获取 Get 连接的请求
 		ctx.JSON(http.StatusOK, gin.H{"msg": "pong"})
 	})
-	r.GET("someGet", someGet)
-	r.GET("getKey", getKey)
-	r.GET("getParam/:username", getParam)
-	r.GET("getParam/:username/:age", getParams)
-	r.POST("somePost", somePost)
-	r.POST("postVal", postVal)
+	r.GET("/someGet", someGet)
+	r.GET("/getKey", getKey)
+	r.GET("/getParam/:username", getParam)
+	r.GET("/getParam/:username/:age", getParams)
+	r.POST("/somePost", somePost)
+	r.POST("/postVal", postVal)
+	r.POST("/search", search)
 	r.LoadHTMLGlob("templates/*")
 	fmt.Println("gin ... ")
 	if err := r.Run(); err != nil { // 开启服务 默认监听127.0.0.1:8080
