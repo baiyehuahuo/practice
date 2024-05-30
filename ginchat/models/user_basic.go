dcc4d986faaabe6ac2c9a03a754ad60fe924634b
fwf_mac <1770194225@qq.com>
2024-05-30 20:49:24 +0800

    finish class 2

--- ginchat/models/user_basic.go
+++ ginchat/models/user_basic.go
@@ -1,21 +1,24 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type UserBasic struct {
 	gorm.Model
-	Name          string
-	Password      string
-	Phone         string
-	Email         string
-	Identity      string
-	ClientIP      string
-	ClientPort    string
-	LoginTime     uint64
-	HeartBeatTime uint64
-	LoginOutTime  uint64
-	IsLoginOut    bool
-	DeviceInfo    string
+	Name          string    `gorm:"column:name" json:"name"`
+	Password      string    `gorm:"column:password" json:"password"`
+	Phone         string    `gorm:"column:phone" json:"phone"`
+	Email         string    `gorm:"column:email" json:"email"`
+	Identity      string    `gorm:"column:identity" json:"identity"`
+	ClientIP      string    `gorm:"column:client_ip" json:"client_ip"`
+	ClientPort    string    `gorm:"column:client_port" json:"client_port"`
+	LoginTime     time.Time `gorm:"column:login_time" json:"login_time"`
+	HeartBeatTime time.Time `gorm:"column:heart_beat_time" json:"heart_beat_time"`
+	LoginOutTime  time.Time `gorm:"column:login_out_time" json:"login_out_time"`
+	IsLoginOut    bool      `gorm:"column:is_login_out" json:"is_login_out"`
+	DeviceInfo    string    `gorm:"column:device_info" json:"device_info"`
 }
 
 func (us *UserBasic) TableName() string {
